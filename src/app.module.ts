78dad1ae86fb8b3cd739f005d1439b5cf0f239e1
ezy4me <mrronal@mail.ru>
2025-02-19 09:10:41 +0300

    generated base modules: database, auth, user, employee, client, appointment, service, category

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { AppointmentModule } from './modules/appointment/appointment.module';
+import { ServiceModule } from './modules/service/service.module';
+import { CategoryModule } from './modules/category/category.module';
+import { EmployeeModule } from './modules/employee/employee.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, AppointmentModule, ServiceModule, CategoryModule, EmployeeModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
